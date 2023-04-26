c066767cb118e3a1304da3568ff866825ddf2b30
marcelmedeiros1 <131443301+marcelmedeiros1@users.noreply.github.com>
2023-04-26 21:45:40 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -1,4 +1,4 @@
-#include "image.hpp"
+#include "Image.hpp"
 
 namespace prog
 {
@@ -6,10 +6,25 @@ namespace prog
   {
     this->width_ = w;
     this->height_ = h;
-    this->color_ = fill;
+    this->pixels_ = new Color*[h];
+    for (int i = 0; i < h; i++) {
+            pixels_[i] = new Color[w];
+        }
+        
+    for(int i=0; i<w; i++){
+      for(int j=0; j<h; j++){
+        pixels_[i][j] = fill;
+      }
+    }
   }
   Image::~Image()
   {
+   // Free rows
+        for (int i = 0; i < height_; i++) {
+            delete[] pixels_[i];
+        }
+        // Free columns
+        delete[] pixels_;
   }
   //A member function that, once called, returns the width_ attribute.
   int Image::width() const
@@ -22,17 +37,18 @@ namespace prog
     return this->height_;
   }
 
-  // TODO: remove this DUMMY_color variable once you have appropriate fields for representing image pixels.
-  Color DUMMY_color;
-
+  //A member function that, once called, gets the mutable reference to the value of pixel (x, y).
   Color& Image::at(int x, int y)
   {
-    
-    return DUMMY_color;
-  }
+  if(x<0 || x>=width_ || y < 0 || y >= height_) throw std::invalid_argument("Coordenadas de pixel inválidas.");
 
+  return pixels_[y][x];
+
+  }
+//A member function that, once called, gets read-only reference to the value of pixel (x, y).
   const Color& Image::at(int x, int y) const
   {
-    return DUMMY_color;
+    if(x<0 || x>=width_ || y < 0 || y >= height_) throw std::invalid_argument("Coordenadas de pixel inválidas.");
+    return pixels_[y][x];
   }
 }
