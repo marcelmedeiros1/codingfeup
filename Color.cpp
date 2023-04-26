8ea2dae4e46b437d3ac91708af13ddf21dac521e
marcelmedeiros1 <131443301+marcelmedeiros1@users.noreply.github.com>
2023-04-26 21:46:39 +0100

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -8,7 +8,7 @@ namespace prog {
         this->green_ = 0;
         this->red_ = 0;
     }
-    //Copy constructor. Receives as argument a constant object of type Color, defines as atributes of the current object a copy of the respective atributes.
+    //Copy constructor. Receives as argument a constant object of type Color, defines as atributes of the current object a copy of the respective attributes.
     Color::Color(const Color& other) {
         this->red_ = other.red_;
         this->green_ = other.green_;
@@ -20,27 +20,27 @@ namespace prog {
         this->green_ = green;
         this->blue_ = blue;
     }
-    //A member function that, once called, returns the red_ atribute.
+    //A member function that, once called, returns the red_ attribute.
     rgb_value Color::red() const {
         return this->red_;
     }
-    //A member function that, once called, returns the green_ atribute.
+    //A member function that, once called, returns the green_ attribute.
     rgb_value Color::green() const {
         return this->green_;
     }
-    //A member function that, once called, returns the blue_ atribute.
+    //A member function that, once called, returns the blue_ attribute.
     rgb_value Color::blue() const {
         return this->blue_;
     }
-    //A member function that, once called, returns a mutable reference of the red_ atribute
+    //A member function that, once called, returns a mutable reference of the red_ attribute
     rgb_value& Color::red()  {
         return this->red_;
     }
-    //A member function that, once called, returns a mutable reference of the green_ atribute
+    //A member function that, once called, returns a mutable reference of the green_ attribute
     rgb_value& Color::green()  {
       return this->green_;
     }
-    //A member function that, once called, returns a mutable reference of the blue_ atribute
+    //A member function that, once called, returns a mutable reference of the blue_ attribute
     rgb_value& Color::blue()  {
       return this->blue_;
     }
