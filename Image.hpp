91aa02609f723a19f34801dfc50193e399fb79cb
marcelmedeiros1 <131443301+marcelmedeiros1@users.noreply.github.com>
2023-04-26 20:09:29 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -7,14 +7,19 @@ namespace prog
   class Image
   {
   private:
-    // TODO: define private fields for image state
+  //Class Atributes, all of type int
+    int width_;
+    int height_;
   public:
+  //Constructors of the class
     Image(int w, int h, const Color &fill = {255, 255, 255});
     ~Image();
+  //Member functions of the class
     int width() const;
     int height() const;
     Color &at(int x, int y);
     const Color &at(int x, int y) const;
   };
 }
+
 #endif
